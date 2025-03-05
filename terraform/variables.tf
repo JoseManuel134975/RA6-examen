c4260941006f586dbc34811758b4c28f4be3a37c
José Manuel <josemanuel3606@gmail.com>
2025-03-05 08:49:56 +0100

    Ejercicios 1 y 2 de RA6

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+# Variable definida
+variable "aws_region" {
+  description = "AWS region"
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "session_token" {
+  type = string
+}
